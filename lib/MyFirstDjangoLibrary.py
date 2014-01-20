ca127a439a0f2358e36fc4cb23618ebd77477fc2
tisto <tisto@plone.org>
2014-01-20 13:03:14 +0100

    Add keywords to start/stop django.

--- lib/MyFirstDjangoLibrary.py
+++ lib/MyFirstDjangoLibrary.py
@@ -6,10 +6,18 @@ to the only \\`Keyword\\`.
 __ http://docutils.sourceforge.net
 """
 
+from robot.api import logger
+
+import os
+import signal
+import subprocess
+
 ROBOT_LIBRARY_DOC_FORMAT = 'reST'
 
 
 class MyFirstDjangoLibrary:
+
+    django_process_pid = None
 
     # TEST CASE => New instance is created for every test case.
     # TEST SUITE => New instance is created for every test suite.
@@ -17,20 +25,26 @@ class MyFirstDjangoLibrary:
     ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
 
     def __init__(self, host="127.0.0.1", port=8000):
-        print("INIT")
+        logger.info("INIT", also_console=True)
 
     def start_django(self):
-        """**Nothing** to see here. Not even in the table below.
-
-        =======  =====  =====
-        Table    here   has
-        nothing  to     see.
-        =======  =====  =====
-        """
-        print("START DJANGO")
+        args = [
+            'python',
+            'mysite/manage.py',
+            'runserver',
+            '--nothreading',
+            '--noreload',
+        ]
+        self.app = subprocess.Popen(args)
+        self.django_process_pid = self.app.pid
+        logger.info(
+            "Django started (PID: %s)" % self.app.pid,
+            also_console=True
+        )
 
     def stop_django(self):
-        print("STOP DJANGO")
-
-    def my_keyword(self):
-        print('Hello, world!')
+        os.kill(self.django_process_pid, signal.SIGKILL)
+        logger.info(
+            "Django stopped (PID: %s)" % self.django_process_pid,
+            also_console=True
+        )
--- test.robot
+++ test.robot
@@ -9,15 +9,19 @@ ${BROWSER}              firefox
 Documentation   Django Robot Tests
 Library         Selenium2Library  timeout=10  implicit_wait=0.5
 Library         MyFirstDjangoLibrary
-Suite Setup     Open Browser To Login Page
-Suite Teardown  Close Browser
+Suite Setup     Start Django and Open Browser
+Suite Teardown  Stop Django and Close Browser
 
 
 *** Keywords ***
 
-Open Browser To Login Page
+Start Django and open Browser
+  Start Django
   Open Browser  ${SERVER}  ${BROWSER}
-  Maximize Browser Window
+
+Stop Django and close browser
+  Close Browser
+  Stop Django
 
 
 *** Test Cases ***
@@ -27,6 +31,3 @@ Scenario: As a visitor I can visit the django default page
   Wait until page contains element  id=explanation
   Page Should Contain  It worked!
   Page Should Contain  Congratulations on your first Django-powered page.
-  My Keyword
-  Start Django
-  Stop Django
