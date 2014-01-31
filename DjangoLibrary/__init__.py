daff9afcde9fa85a1feb4a9173310f59a82e93e0
tisto <tisto@plone.org>
2014-01-31 12:31:01 +0100

    Implement create_user keyword. Needs to be refactored.

--- DjangoLibrary/__init__.py
+++ DjangoLibrary/__init__.py
@@ -4,6 +4,7 @@ __version__ = '0.1'
 from robot.api import logger
 
 import os
+import sys
 import signal
 import subprocess
 
@@ -26,8 +27,42 @@ class DjangoLibrary:
         self.host = host
         self.port = port
 
+    def clear_db(self):
+        # XXX: Flush seems to be not working
+        #args = [
+        #    'python',
+        #    'mysite/manage.py',
+        #    'flush',
+        #    '--noinput',
+        #]
+        args = [
+            'rm',
+            'mysite/db.sqlite3',
+        ]
+        subprocess.Popen(args)
+        args = [
+            'python',
+           'mysite/manage.py',
+            'syncdb',
+            '--noinput',
+        ]
+        subprocess.Popen(args)
+
+    def create_user(self, username, email, password):
+        sys.path.append(os.path.dirname(os.path.realpath('mysite/mysite')))
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+        from django.contrib.auth.models import User
+        user = User.objects.create_superuser(
+            'admin',
+            email='admin@admin.com',
+            password='admin'
+        )
+        user.save()
+        logger.console("Create User")
+
     def start_django(self):
         """Start the Django server."""
+        self.clear_db()
         args = [
             'python',
             'mysite/manage.py',
@@ -40,6 +75,10 @@ class DjangoLibrary:
         logger.console(
             "Django started (PID: %s)" % self.django_pid,
         )
+        # XXX: This is ugly as hell and needs to be refactored!!!
+        from time import sleep
+        sleep(10)
+        self.create_user("admin", "admin@admin.com", "admin")
 
     def stop_django(self):
         """Stop Django server."""
@@ -47,4 +86,3 @@ class DjangoLibrary:
         logger.console(
             "Django stopped (PID: %s)" % self.django_pid,
         )
-#
---
+++ DjangoLibrary/tests/autologin.robot
@@ -0,0 +1,41 @@
+*** Variables ***
+
+${SERVER}               http://localhost:55001
+${BROWSER}              firefox
+
+
+*** Settings ***
+
+Documentation   Django Robot Tests
+Library         Selenium2Library  timeout=10  implicit_wait=0.5
+Library         DjangoLibrary  127.0.0.1  55001
+Suite Setup     Start Django and Open Browser
+Suite Teardown  Stop Django and Close Browser
+
+
+*** Keywords ***
+
+Start Django and open Browser
+  Start Django
+  Open Browser  ${SERVER}  ${BROWSER}
+
+Stop Django and close browser
+  Close Browser
+  Stop Django
+
+Pause
+  Import library  Dialogs
+  Pause execution
+
+
+*** Test Cases ***
+
+Scenario: As a administrator I can log myself in
+  Go To  ${SERVER}/admin
+  Wait until page contains  Django administration
+  Input text  username  admin
+  Input text  password  admin
+  Click Button  Log in
+  Wait until page contains  Django administration
+  Page should contain  Django administration
+  Page should not contain  Please enter the correct username and password
