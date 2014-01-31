af5848694ab185e53a8077fddefff8a2915523cd
tisto <tisto@plone.org>
2014-01-31 10:07:47 +0100

    Flake8

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
