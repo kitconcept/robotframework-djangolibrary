8e8b6297cbd11cce090a0b18aa99c0d022e195a0
tisto <tisto@plone.org>
2014-01-31 09:57:39 +0100

    Pep8 setup.py; Add requirements to setup.py.

--- setup.py
+++ setup.py
@@ -3,24 +3,32 @@ import sys, os
 
 version = '0.1'
 
-setup(name='robotframework-djangolibrary',
-      version=version,
-      description="A robot framework library for Django.",
-      long_description="""\
+setup(
+    name='robotframework-djangolibrary',
+    version=version,
+    description="A robot framework library for Django.",
+    long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='robotframework django test',
-      author='Timo Stollenwerk',
-      author_email='tisto@plone.org',
-      url='http://timostollenwerk.net',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='robotframework django test',
+    author='Timo Stollenwerk',
+    author_email='tisto@plone.org',
+    url='http://timostollenwerk.net',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Django',
+        'robotframework',
+        'robotframework-selenium2library',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
