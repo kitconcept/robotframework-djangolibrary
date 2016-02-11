21fb1463c4d7f4cffcb2546b29e15a4498d722e7
Timo Stollenwerk <tisto@plone.org>
2016-02-11 16:09:26 +0100

    Fix issue with py3.

--- DjangoLibrary/middleware.py
+++ DjangoLibrary/middleware.py
@@ -13,8 +13,12 @@ class AutologinAuthenticationMiddleware(AuthenticationMiddleware):
             auth.logout(request)
             return
         autologin_cookie_value = base64.b64decode(request.COOKIES['autologin'])
-        username = autologin_cookie_value.split(':')[0]
-        password = autologin_cookie_value.split(':')[1]
+        try:
+            username = autologin_cookie_value.split(':')[0]
+            password = autologin_cookie_value.split(':')[1]
+        except TypeError:
+            username = autologin_cookie_value.decode('utf8').split(':')[0]
+            password = autologin_cookie_value.decode('utf8').split(':')[1]
         user = auth.authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
