bb78a115a6ed479f6f8d0fedd6664d2395c86680
Timo Stollenwerk <tisto@plone.org>
2016-03-31 16:16:40 +0200

    Flake8.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -59,8 +59,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'robotframework-djangolibrary',
         'USER': 'robotframework-djangolibrary',
