a0704df0316bc97e6908c24ff34fd5579bdcc84b
Timo Stollenwerk <tisto@plone.org>
2016-03-31 17:20:48 +0200

    Revert "Intentionally break psql on travis to see if we test the real thing."
    This reverts commit 180589b849332511bdfd9c69dc509c7783d1ca6d.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -94,7 +94,7 @@ if 'TRAVIS' in os.environ:
         DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                'NAME': 'travis_ci_test_AA',
+                'NAME': 'travis_ci_test',
                 'USER': 'postgres',
                 'PASSWORD': '',
                 'HOST': 'localhost',
