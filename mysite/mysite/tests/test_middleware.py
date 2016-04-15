2198e43a3701351085ac186a9a8574b788148fcf
Timo Stollenwerk <tisto@plone.org>
2016-04-15 14:06:28 +0200

    Add unit test for factory boy middleware.

--- mysite/mysite/tests/test_middleware.py
+++ mysite/mysite/tests/test_middleware.py
@@ -3,16 +3,32 @@ from django.test import TestCase
 from DjangoLibrary.middleware import FactoryBoyMiddleware
 from mock import Mock
 
+import json
+
 
 class TestFactoryBoyMiddleware(TestCase):
 
     def setUp(self):
-        self.cm = FactoryBoyMiddleware()
+        self.middleware = FactoryBoyMiddleware()
         self.request = Mock()
         self.request.session = {}
 
     def test_process_request_creates_object(self):
-        setattr(self.request, 'FACTORY_BOY_MODEL_PATH', 'User')
-        self.assertEqual(self.cm.process_request(self.request), None)
+        self.request.configure_mock(
+            **{
+                'GET': {
+                    'FACTORY_BOY_MODEL_PATH': 'mysite.tests.factories.UserFactory',  # noqa
+                    'FACTORY_BOY_ARGS': ''
+                }
+            }
+        )
+
+        response = self.middleware.process_request(self.request)
+
+        self.assertEqual(201, response.status_code)
+        self.assertEqual(
+            'johndoe',
+            json.loads(response.content).get('username')
+        )
         self.assertEqual(1, len(User.objects.values()))
         self.assertEqual('johndoe', User.objects.values()[0]['username'])
